04789fdf199a4070ef246e3fc1f0ab5197e6b4c1
Khushi Jain <khushij393@gmail.com>
2022-01-06 13:57:18 +0530

    removed mongo db credentials

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(cookieParser())
 app.set('view engine', 'ejs');
 
 // database connection
-const dbURI = 'mongodb+srv://blackwidow:Uff3ThnyAkCYqBA@cluster0.jh2c3.mongodb.net/jwt_auth';
+const dbURI = 'mongodb+srv://username:password@cluster0.jh2c3.mongodb.net/jwt_auth';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
